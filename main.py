eada3b41bc56dd30a33cbed2f2a9080d59ded8ee
NabilRaiyan <rraiyan77@gmail.com>
2024-08-19 17:26:32 +0600

    creating post route for asking question to the llama3 model

--- main.py
+++ main.py
@@ -6,50 +6,24 @@ from uuid import UUID, uuid4
 
 app = FastAPI()
 
-class Task(BaseModel):
+class Bot(BaseModel):
     id: Optional[UUID] = None
-    title: str
-    description: Optional[str] = None
-    completed: bool = False
-
-tasks = []
+    question: str
+    answer: str
 
 
-@app.post("/tasks/", response_model = Task)
-def create_task(task: Task):
-    task.id = uuid4()
-    tasks.append(task)
-    return task
-
-@app.get("/get-tasks/", response_model = List[Task])
-def read_task():
-    return tasks
+responses = []
 
 
-@app.get("/tasks/{task_id}", response_model = Task)
-def read_task(task_id: UUID):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-    raise HTTPException(status_code = 404, detail="Task not found")
+@app.post("/ask-question/", response_model = Bot)
+def create_bot(bot: Bot):
+    bot.id = uuid4()
+    responses.append(bot)
+    return bot
 
-@app.put("/tasks/{task_id}", response_model = Task)
-def update_task(task_id: UUID, task_update: Task):
-    for idx, task in enumerate(tasks):
-        if task.id == task_id:
-            updated_task = task.copy(update = task_update.dict(exclude_unset=True))
-            tasks[idx] = updated_task
-            return updated_task
-    raise HTTPException(status_code=404, detail="Task not found")
-
-
-
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id):
-    for idx , task in enumerate(tasks):
-        if task.id == task_id:
-            return task.pop(idx)
-    raise HTTPException(status_code=404, detail="Task not found")
+@app.get("/get-responses/", response_model = List[Bot])
+def read_bot():
+    return responses
 
 
 if __name__ == "__main__":
