950584c2b127969d7421f934db6bf89b1325df5c
NabilRaiyan <rraiyan77@gmail.com>
2024-10-03 22:32:16 +0600

    creating db

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+# mysql database url
 URL_DATABASE = 'mysql+pymysql://root:Qwerty123key###a@localhost:3306/BlogApplication'
+# creating engine
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
