a1f47c390ac0f95635a45c334ef4cccc778519d8
NabilRaiyan <rraiyan77@gmail.com>
2024-10-04 21:36:41 +0600

    creating db

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from database import Base
 class User(Base):
     __tablename__ = 'users'
     
+    # creating columns
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True)
 
